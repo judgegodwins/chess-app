493cf65cc910426912472b6c7893ddfc8cd8e061
Judge Godwins <judgegodwins@gmail.com>
2023-09-05 09:06:17 +0100

    add descriptive loading message

--- src/pages/Verify.tsx
+++ src/pages/Verify.tsx
@@ -2,22 +2,38 @@ import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../hooks/redux";
 import { verifyToken } from "../slices/authSlice";
+import { CircularProgress, Stack, Typography } from "@mui/material";
 
 export default function Verify() {
-
   const dispatch = useAppDispatch();
 
   const auth = useAppSelector(({ auth }) => auth);
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
 
     if (!token) return;
 
     dispatch(verifyToken());
-  }, [dispatch]);                                                                                                                                                                         
+  }, [dispatch]);
 
-  return (
-    auth.status === 'verifying' ? <div>Verifying...</div> : <Outlet />
+  return auth.status === "verifying" ? (
+    <Stack
+      sx={{
+        width: "100vw",
+        height: "100vh",
+      }}
+      alignItems="center"
+      justifyContent="center"
+      gap={2}
+    >
+      <CircularProgress />
+      <Typography textAlign="center">
+        Please wait while the app loads 🥹. The backend server is deployed on a
+        free instance and is probably just waking up.
+      </Typography>
+    </Stack>
+  ) : (
+    <Outlet />
   );
 }
